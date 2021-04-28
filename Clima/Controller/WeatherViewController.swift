af6510e16a12e4ca558fad7c84b60b41b8a09535
Mariia Karalash <>
2021-04-28 23:11:18 +0300

    Created a WeatherModel

---
+++ Clima/Controller/WeatherViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  Clima
+//
+//  Created by Angela Yu on 01/09/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManagerDelegate {
+
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    var weatherManager = WeatherManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        weatherManager.delegate = self
+        
+        searchTextField.delegate = self
+    }
+
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if searchTextField.text != ""{
+            return true
+        } else {
+            searchTextField.placeholder = "Type something"
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        if let city = searchTextField.text {
+            weatherManager.fetchWeather(cityName: city)
+        }
+        searchTextField.text = ""
+    }
+    
+    func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel) {
+        temperatureLabel.text = weather.temperatureString
+        
+    }
+    
+    func didFailWithError(error: Error) {
+        print(error)
+    }
+    
+}
+
