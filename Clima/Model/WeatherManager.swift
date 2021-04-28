739dfd9fdbf7e89c981a0c82ae023eafcfa07ee2
Mariia Karalash <>
2021-04-28 23:08:52 +0300

    Initial commit

---
+++ Clima/Model/WeatherManager.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by Ihor Karalash on 28.04.2021.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
---
+++ Clima/Model/WeatherModel.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherModel.swift
+//  Clima
+//
+//  Created by Ihor Karalash on 28.04.2021.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
